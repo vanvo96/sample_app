899fd44e6918da2386d514378e08e6b60fc6b66a
vanvo96 <vo.trong.van@framgia.com>
2018-08-27 09:12:56 +0700

    Chapter 12: Password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_005307) do
+ActiveRecord::Schema.define(version: 2018_08_27_055055) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 2018_08_27_005307) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.string "reset_digest"
   end
 
 end
